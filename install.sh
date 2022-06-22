3579e3518e7a00aaad10ee62a1e4f62eac8e2de3
Chris Ziraldo <chris.ziraldo@youi.tv>
2022-06-22 09:38:55 -0400

    Adding install script

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+platform='unknown'
+unamestr=$(uname)
+
+if [ "$unamestr" == 'Linux' ]; then
+	platform='Linux'
+
+	echo Installing jq
+	sudo apt-get install jq
+
+elif [ "$unamestr" == 'Darwin' ]; then
+	platform='Darwin'
+
+	echo Install Homebrew
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+	echo Validate Homebrew
+	brew doctor
+
+	echo Install Homebrew Packages
+	brew install jq
+
+fi
+
+if [ "$platform" == 'unknown' ]; then
+	echo "Currently only Linux and Darwin platforms are supported"
+	exit 1
+fi
+
+url='https://raw.githubusercontent.com/ChrisZiraldo/cvm/main/cvm'
+
+folder_name=cmake-${version}
+cvm_home=${HOME}/.cvm
+
+if [ ! -d "${cvm_home}" ]; then
+	echo "creating ${cvm_home}"
+	mkdir ${cvm_home}
+	mkdir ${cvm_home}/"cmake-active"
+fi
+
+if [ ! -d "${cvm_home}/${folder_name}" ]; then
+	pushd ${cvm_home} >/dev/null
+	curl -L -o 'cvm' ${url}
+	popd >/dev/null
+fi
+
+if [[ "$PATH" != *".cvm"* ]]; then
+	echo "export PATH=${cmake_bin}:${cvm_home}:\${PATH}" >${cvm_home}/cvm_source
+	echo "Done! Please place the following line in your .bash_profile, .zshrc, .bashrc or .profile"
+	echo
+	echo "	source $HOME/.cvm/cvm_source"
+fi
